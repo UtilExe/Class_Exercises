97aef6a2c78636a8d440d96e65a02e0a365c7480
UtilExe <laymasterlolz@gmail.com>
2021-02-12 10:17:26 +0100

    Class exercises. Implement task 1 and 2: List all files in a folder. And read all lines in a file, and copy the lines that starts with a number.

---
+++ 02-Exercises-Extra/02_2_files.py
@@ -0,0 +1,34 @@
+# Exercise read and write
+# 1. create a function in python, that can read all names of files in a folder, when given the full path to the folder
+print("Task 1: ")
+import os
+def readNamesOfFilesInFolder(path):
+    entries = os.listdir(path)
+
+    for entry in entries:
+        print(entry)
+
+readNamesOfFilesInFolder('C:/Users/Emil/Desktop/Skole/docker');
+
+
+print("Task 2: ")
+import re # used for the regression matcher.. there may be a better way..
+#2. create a function, that can read all lines from a file and copy to another file only the lines, that starts with a number
+
+
+intLines = []
+def readLinesToNumb(filename) :
+    with open(filename,'r') as file_object:
+        lines = file_object.readlines()
+    value = 0
+    for line in lines:
+        if re.match(r"^\d+.*$",line): # was needed to identify if the starting value was a number. isdigit did not work. and int convertion would crash when it was a string. maybe there's a better way?
+            intLines.append(line)
+
+def copyToOtherFile(filename, array):
+    with open(filename, 'w') as file_object:
+        for numb in array:
+            file_object.write(numb)
+
+readLinesToNumb('C:/Users/Emil/Desktop/Skole\Python/Materiale/docker_notebooks/notebooks/my_notebooks/02-Exercises-Extra/data.txt')
+copyToOtherFile('C:/Users/Emil/Desktop/Skole\Python/Materiale/docker_notebooks/notebooks/my_notebooks/02-Exercises-Extra/datanumbers.txt', intLines)
